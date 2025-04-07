0e7f86aa6ad1f3176ae1f4c72ae5843f5c0079e2
harsh8957 <harshsing8957@gmail.com>
2025-04-07 16:53:22 +0530

    hello ji

---
+++ vaidikmaster.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void optimizeIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {
+    optimizeIO();
+    int t;
+    cin >> t;
+    while (t--) {
+        int a,b;
+        cin>>a>>b;
+        int ans = a+b;
+        cout<<ans<<endl;
+        //here we are just adding 2 numbers for harsh pata nhi kyu pagal hai sala 
+        //ok
+     
+      
+      
+      
+    }
+    return 0;
+}
