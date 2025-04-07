ec4c9e6719c007fc227aea1745179edf287abef6
harsh8957 <harshsing8957@gmail.com>
2025-04-07 16:48:47 +0530

    hanji

---
+++ harsh.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void optimizeIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {
+    optimizeIO();
+    int t;
+    cin >> t;
+    while (t--) {
+    std::cout << "Harsh Bhai" << std::endl;
+     
+      
+      
+      
+    }
+    return 0;
+}
